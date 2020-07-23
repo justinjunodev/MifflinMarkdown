bb86b2fda46b2057f0304cc8d4ab127bd6fd6672
Justin Juno <hello@justinjuno.dev>
2020-07-23 14:51:16 -0500

    add and render react-markdown

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const Main = () => {
+    const [markdown, setMarkdown] = useState('# Hello, Markdown!')
+
+    function handleChange(e) {
+        setMarkdown(e.target.value)
+    }
+
     return (
         <main>
-           <div>Markdown Input</div> 
-           <div>Markdown Preview</div>
+            <div>
+                <textarea className="editor" onChange={handleChange} value={markdown} />
+            </div>
+            <ReactMarkdown className="preview" source={markdown} />
         </main>
     )
 }
