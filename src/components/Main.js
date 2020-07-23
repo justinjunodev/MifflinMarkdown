10616daa217dfd57b3dc308890033bf5e6a7503d
Justin Juno <hello@justinjuno.dev>
2020-07-23 16:16:07 -0500

    add feature for downloading md file

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,20 @@ const Main = () => {
         setMarkdown(e.target.value)
     }
 
+    function handleDownload() {
+        const source = document.createElement("a");
+        const file = new Blob([markdown], { type: 'text/plain' });
+        source.href = URL.createObjectURL(file);
+        source.download = 'mifflin.md';
+        source.click();
+    }
+
     return (
         <main>
             <div>
+                <div className="options">
+                    <button onClick={handleDownload} type="button">Download</button>
+                </div>
                 <textarea className="editor" onChange={handleChange} value={markdown} />
             </div>
             <ReactMarkdown className="preview" source={markdown} />
