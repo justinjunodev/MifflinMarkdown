ca8a39796379424ca2b50055e023f69692dcad52
Justin Juno <hello@justinjuno.dev>
2020-07-24 16:41:31 -0500

    add footer styles and content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
 import React from "react"
+import styled from "styled-components"
 
 const Footer = () => {
-  return <footer>Footer Area</footer>
+  return (
+    <FooterWrapper>
+      <h2>Mifflin.md</h2>
+      <h3>A Markdown Company</h3>
+      <p>All Rights Reserved &copy; {new Date().getFullYear()}</p>
+    </FooterWrapper>
+  )
 }
 
 export default Footer
+
+// Component Styles
+const FooterWrapper = styled.footer`
+  display: flex;
+  flex-flow: column;
+  place-content: center;
+  text-align: center;
+  color: var(--light);
+  height: 150px;
+  h2,
+  h3 {
+    font-family: var(--sans);
+    text-transform: uppercase;
+    margin: 0;
+  }
+  p {
+    margin: 0.5rem 0 0;
+    font-size: 0.75rem;
+    opacity: 0.75;
+  }
+`
