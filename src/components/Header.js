4c07f8007b67409a13622853962054e2bd405441
Justin Juno <hello@justinjuno.dev>
2020-07-24 11:17:38 -0500

    add quotes and render in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from "react"
+import quotes from "../data/quotes"
 
 const Header = () => {
-  return <header>Header Area</header>
+  return (
+    <header>
+      <div>Header</div>
+      <aside>
+        <blockquote>&quot;{quotes[0]}&quot;</blockquote>
+      </aside>
+    </header>
+  )
 }
 
 export default Header
---
+++ src/data/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  "Sometimes I’ll start a sentence, and I don’t even know where it’s going. I just hope I find it along the way.",
+  "I guess I’ve been working so hard, I forgot what it’s like to be hardly working.",
+  "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked, but it’s not like this compulsive need to be liked, like my need to be praised.",
+  "And I knew exactly what to do. But in a much more real sense, I had no idea what to do.",
+  "I am running away from my responsibilities. And it feels good.",
+  "Sometimes you have to take a break from being the kind of boss that’s always trying to teach people things. Sometimes you just have to be the boss of dancing.",
+  "Ok, too many different words from coming at me from too many different sentences.",
+  "Oh, this is gonna feel so good getting this thing off my chest… that’s what she said.",
+  "My mind is going a mile an hour.",
+  "And I’m optimistic because everyday I get a little more desperate.",
+]
+
+export default quotes
